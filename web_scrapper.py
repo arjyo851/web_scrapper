b6029ec48cd9cebffe4edacf21ab200dfad7dd89
arjyo851 <arjyo77@gmail.com>
2021-07-03 17:21:03 +0530

    changing it back to github user from instagram user

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-github_user = input('Input Github user:')
-url = 'https://github.com/'+github_user
+github_user = input('Input github user:')
+url = 'https://www.github.com/{}/'.format(github_user)
 r= requests.get(url)
 soup = bs(r.content,'html.parser')
+print(soup.prettify)
 profile_image = soup.find('img',{'alt' : 'Avatar'})['src']
 print(profile_image)
